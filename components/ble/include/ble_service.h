18598468c7ca485e1723e67b5161ee03067791ad
administrator <reachnikhilc@gmail.com>
2026-01-31 17:33:52 -0500

    more cleanup and added 9600 folder to gitignore

--- components/ble/include/ble_service.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "esp_err.h"
-#ifdef __cplusplus
-extern "C" {
-#endif
-esp_err_t ble_init_if_enabled(void); // no-op unless FORMSYNC_ENABLE_BLE=y
-#ifdef __cplusplus
-}
-#endif
